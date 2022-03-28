8e4a3b79f4c9614082625d6b3df3ecb1963ad0fc
christian <christian.viot2021@campus-eni.fr>
2022-03-28 11:47:57 +0200

    Fonctions, Actions et expressions lambda

---
+++ DemoLambda/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var joursAvantNoel = new Func<DateTime, int>(date =>
+              {
+                  var noel = new DateTime(date.Year, 12, 25);
+                  if (noel<date)
+                  {
+                     noel= noel.AddYears(1);
+                  }
+                  return (noel - date).Days;
+              });
+            var days = joursAvantNoel(DateTime.Now);
+            var dateFinAnnee = new DateTime(DateTime.Now.Year, 12, 31);
+            Console.WriteLine($"Le nombre de jours avant noël à partir d'aujourd'hui est de {days}");
+            Console.WriteLine($"Le nombre de jours avant noël à partir du 31 décembre {joursAvantNoel(dateFinAnnee)}");
+
+            var actionJoursAvantNoel = new Action<DateTime>(date =>
+              {
+                  var noel = new DateTime(date.Year, 12, 25);
+                  if (noel < date)
+                  {
+                      noel = noel.AddYears(1);
+                  }
+                  Console.WriteLine($"Le nombre de jours avant noël à partir du {date.ToShortDateString()} est de {(noel - date).Days} jours");
+               
+              });
+            actionJoursAvantNoel(DateTime.Now);
+            actionJoursAvantNoel(dateFinAnnee);
+            Console.ReadKey();
+        }
+    }
+}
