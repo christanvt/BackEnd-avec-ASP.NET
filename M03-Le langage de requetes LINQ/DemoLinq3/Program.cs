ced4f2fdbc129203be5ed07a99b0f8a4601ab4ed
christian <christian.viot2021@campus-eni.fr>
2022-03-28 15:00:28 +0200

    Vérifier, trnasformer, ranger une collection

---
+++ M03-Le langage de requetes LINQ/DemoLinq3/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLinq3
+{
+    public class Rue
+    {
+        public string Nom { get; set; }
+        public int Numero { get; set; }
+    }
+
+    public class Appartement
+    {
+        public int Numero { get; set; }
+        public List<Piece> Pieces { get; set; }
+    }
+
+    public class Piece
+    {
+        public string TypePiece { get; set; }
+        public int surface { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listeDeRues = new List<Rue>
+            {
+                new Rue{Nom = "Rue des lilas", Numero = 1},
+                new Rue{Nom = "Rue des monts", Numero = 13},
+                new Rue{Nom = "Rue des lilas", Numero = 50},
+                new Rue{Nom = "Rue des lilas", Numero = 114},
+                new Rue{Nom = "Rue des monts", Numero = 25},
+                new Rue{Nom = "Rue des asperges", Numero = 8},
+                new Rue{Nom = "Rue des xylophones", Numero = 47}
+            };
+            var orderedByNames = listeDeRues.OrderBy(r => r.Nom);
+            Console.WriteLine("Liste des rues triée sur le nom :");
+            foreach (var rue in orderedByNames)
+            {
+                Console.WriteLine($"Nom = {rue.Nom}, numéro = {rue.Numero}");
+            }
+
+            var orderedByNumberDescending = listeDeRues.OrderByDescending(r => r.Numero);
+            Console.WriteLine();
+            Console.WriteLine("Liste des rues triée sur le numéro en décroissant :");
+            foreach (var rue in orderedByNumberDescending)
+            {
+                Console.WriteLine($"Nom = {rue.Nom}, numéro = {rue.Numero}");
+            }
+
+            var orderedStreet = listeDeRues.OrderBy(r => r.Nom).ThenBy(r => r.Numero);
+            Console.WriteLine();
+            Console.WriteLine("Liste des rues triée sur le nom, puis sur le numéro :");
+            foreach (var rue in orderedStreet)
+            {
+                Console.WriteLine($"Nom = {rue.Nom}, numéro = {rue.Numero}");
+            }
+
+            Console.WriteLine();
+            if (listeDeRues.All(r=>r.Numero<200))
+            {
+                Console.WriteLine("Les numéros des rues sont tous inférieurs à 200 :");
+            }
+
+            Console.WriteLine();
+            if (listeDeRues.Any(r => r.Numero < 100 && r.Numero > 50))
+            {
+                Console.WriteLine("il existe des rues entre 50 et 100 :");
+            }
+
+            var ruesUniques = listeDeRues.Select(r => r.Nom).Distinct().OrderBy(s=>s);
+            Console.WriteLine();
+            Console.WriteLine("Les noms de rues uniques sont : ");
+            foreach (var nom in ruesUniques)
+            {
+                Console.WriteLine(nom);
+            }
+
+            var appartements = new List<Appartement>
+            {
+                new Appartement
+                {
+                    Numero=1,
+                    Pieces=new List<Piece>
+                    {
+                        new Piece { surface=5, TypePiece="Cuisine" },
+                        new Piece{surface=15,TypePiece="Salon"},
+                        new Piece{surface=10,TypePiece="Chambre"}
+                    },
+                },
+
+                new Appartement
+                {
+                    Numero=2,
+                    Pieces=new List<Piece>
+                    {
+                        new Piece {surface=4, TypePiece="Cuisine"},
+                        new Piece {surface=21,TypePiece="Salon"},
+                        new Piece{surface=9,TypePiece="Chambre"}
+                    }
+                },
+
+                 new Appartement
+                {
+                    Numero=3,
+                    Pieces=new List<Piece>
+                    {
+                        new Piece {surface=6,TypePiece="Cuisine"},
+                        new Piece{surface=19, TypePiece="Salon"},
+                        new Piece{surface=8, TypePiece="Chambre"}
+                    }
+                 },
+
+                 new Appartement
+                {
+                    Numero=4,
+                    Pieces=new List<Piece>
+                    {
+                        new Piece {surface=8,TypePiece="Cuisine"},
+                        new Piece{surface=30, TypePiece="Salon"},
+                        new Piece{surface=12, TypePiece="Chambre"}
+                    }
+                 }
+            };
+
+            var pieces = appartements.SelectMany(a => a.Pieces);
+            Console.WriteLine();
+            Console.WriteLine("Voici la liste de toutes les pièces des appartements : ");
+            foreach (var piece in pieces)
+            {
+                Console.WriteLine($"Pièce de type {piece.TypePiece} et de surface {piece.surface}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ M03-Le langage de requetes LINQ/DemoLinq3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("DemoLinq3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DemoLinq3")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("cbb66873-3ec1-4c16-9922-141077e52ea9")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
