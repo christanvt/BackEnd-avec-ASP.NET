01bf2acbc70a475ca1f34e8b92faf4c6cd38d327
christian <christian.viot2021@campus-eni.fr>
2022-03-28 11:24:20 +0200

    Extensions

---
+++ MethodeExtension/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodeExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine($"Est ce que {a} est pair ?{ a.EstPair()}");
+
+            Console.ReadKey();
+
+            var chaine = "iL FAIT beAu Aujourd'hUI";
+            Console.WriteLine(chaine.PremiereLettreMaj());
+            Console.ReadKey();
+        }
+    }
+}
