48ad5534b3828ed5ff9e420ea9561b0ebf8e90f3
christian <christian.viot2021@campus-eni.fr>
2022-03-29 12:38:42 +0200

    TP Les chats

---
+++ M05-ASP.NET Razor/TP/LesChats/Controllers/ChatController.cs
@@ -0,0 +1,66 @@
+﻿using LesChats.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LesChats.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static List<Chat> chats;
+        public List<Chat> Chats => chats ?? (chats = Chat.GetMeuteDeChats());
+
+        // GET: Chat
+        public ActionResult Index()
+        {
+            return View(Chats);
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+            var chat = Chats.FirstOrDefault(c => c.Id == id);
+            if (chat == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(chat);
+        }
+
+               
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var chat = Chats.FirstOrDefault(c => c.Id == id);
+            if (chat == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(chat);
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                var chat = Chats.FirstOrDefault(c => c.Id == id);
+                if(chat != null)
+                {
+                    Chats.Remove(chat);
+                }
+               
+            }
+            catch
+            {
+                return View();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
