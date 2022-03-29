928e912182edca827688886a3a20699211917ee4
christian <christian.viot2021@campus-eni.fr>
2022-03-29 11:09:43 +0200

    Créer une action, une vue et un attribut

--- M04-Le framework ASP.NET MVC/PremierSite/Controllers/HomeController.cs
+++ M04-Le framework ASP.NET MVC/PremierSite/Controllers/HomeController.cs
@@ -26,5 +26,18 @@ namespace PremierSite.Controllers
 
             return View();
         }
+
+        public ActionResult Bonjour()
+        {
+            
+            return View();
+        }
+
+        [Route("Bienvenue/{nom}")]
+        public ActionResult Welcome(string nom)
+        {
+            ViewBag.Nom = nom;
+            return View();
+        }
     }
 }
