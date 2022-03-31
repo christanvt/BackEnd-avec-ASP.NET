27e9f8faf378ec5445dd7fbf37f36ef40aea4cd1
christian <christian.viot2021@campus-eni.fr>
2022-03-31 14:46:13 +0200

    merge

---
+++ M06-Entity Framework/DemoDbContext/Adresse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDbContext
+{
+   public class Adresse
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public int Numero { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Numero} {Nom}"; 
+        }
+    }
+}
