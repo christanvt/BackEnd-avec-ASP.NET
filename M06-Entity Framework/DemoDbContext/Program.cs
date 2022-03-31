59cc552f7b95e347f9a2b60d7430317722c51fb4
christian <christian.viot2021@campus-eni.fr>
2022-03-31 14:12:09 +0200

    Utilisation des migrations, gestion des relations entre entités

--- M06-Entity Framework/DemoDbContext/Program.cs
+++ M06-Entity Framework/DemoDbContext/Program.cs
@@ -12,9 +12,16 @@ namespace DemoDbContext
         {
             var context = new Context();
 
+            //var adresse1 = new Adresse { Nom = "Rue des Lilas", Numero = 42 };
+            //var adresse2 = new Adresse { Nom = "Rue des Mimosas", Numero = 26 };
+
+            //context.Personnes.FirstOrDefault(p => p.Id == 1).Adresse = adresse1;
+            //context.Personnes.FirstOrDefault(p => p.Id == 2).Adresse = adresse2;
+            //context.SaveChanges();
+
             foreach (var personne in context.Personnes)
             {
-                Console.WriteLine($"Nom:{personne.Nom}, Prenom:{personne.Prenom}");
+                Console.WriteLine($"Nom:{personne.Nom}, Prenom:{personne.Prenom}, Adresse:{personne.Adresse}");
             }
 
             if (!context.Personnes.Any())
