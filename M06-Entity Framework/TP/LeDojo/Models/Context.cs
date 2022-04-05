670f95ef13d2a0be93e6057df7a670bf3d44db63
christian <christian.viot2021@campus-eni.fr>
2022-04-05 15:47:52 +0200

    TP LeDojo

---
+++ M06-Entity Framework/TP/LeDojo/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LeDojo.Models
+{
+    public class Context:DbContext
+    {
+        public Context() : base("name=Context")
+        {
+
+        }
+        public System.Data.Entity.DbSet<BO.Samourai> Samourais { get; set; }
+        public System.Data.Entity.DbSet<BO.Arme> Armes { get; set; }
+    }
+}
---
+++ M06-Entity Framework/TP/LeDojo/Models/SamouraiEdition.cs
@@ -0,0 +1,19 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeDojo.Models
+{
+    public class SamouraiEdition
+    {
+  
+            public Samourai Samourai { get; set; }
+            public List<Arme> Armes { get; set; } 
+
+            public int? IdSelectedArme { get; set; }
+       
+    }
+}
